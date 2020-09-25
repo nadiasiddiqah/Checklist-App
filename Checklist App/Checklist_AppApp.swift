369c3aa1cbdec768aaf9622987f7d7a400967420
nadiasiddiqah <nadiasiddiqah@gmail.com>
2020-09-25 12:31:12 -0700

    Initial Commit

---
+++ Checklist App/Checklist_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Checklist_AppApp.swift
+//  Checklist App
+//
+//  Created by Nadia Siddiqah on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Checklist_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
