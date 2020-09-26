0ff90d7acb2c0295253dee47cc426110dc88d60b
nadiasiddiqah <nadiasiddiqah@gmail.com>
2020-09-25 19:16:38 -0700

    Created Navigation View and List View. Added deleting and moving list item functions.

--- Checklist App/ContentView.swift
+++ Checklist App/ContentView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var checklistItems = [
+        "Fold clothes",
+        "Pack for the weekend",
+        "Move boxes to car",
+        "Full body workout",
+        "Start building Checklist app",     /// add comma to end of array to make it easy to update
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(checklistItems, id: \.self) { item in
+                    Text(item)
+                }
+                .onDelete(perform: deleteListItem)      // enables swipe-to-delete gesture
+                .onMove(perform: moveListItem)          // enables move gesture
+            }
+            .navigationBarItems(trailing: EditButton())     // adds edit button method to trailing side of NavigationBar
+            .navigationTitle("Checklist")
+            .onAppear() {       // standard view method to execute code when list first appears
+                self.printChecklistContents()
+            }
+        }
+    }
+    // Method to print checklist in debug log
+    func printChecklistContents() {
+        for item in checklistItems {
+            print(item)
+        }
+    }
+    // Method to remove item in checklist
+    func deleteListItem(whichElement: IndexSet) {       // IndexSet = index position of item swiped on
+        checklistItems.remove(atOffsets: whichElement)      // deletes IndexSet
+        printChecklistContents()
+    }
+    // Method to move item in checklist
+    func moveListItem(whichElement: IndexSet, destination: Int) {       // whichElement = current position
+        checklistItems.move(fromOffsets: whichElement, toOffset: destination)       // destination = position moved to
+        printChecklistContents()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
